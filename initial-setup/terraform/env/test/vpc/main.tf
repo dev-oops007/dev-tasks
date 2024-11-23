90c76b7ba52d1c008d2fcc87f40b4a1a1ca2e1e8
dev-oops007 <tomardev007@gmail.com>
2024-11-23 02:33:37 -0500

    Terraform Independent Resource Directories strategy

--- initial-setup/terraform/env/test/vpc/main.tf
+++ initial-setup/terraform/env/test/vpc/main.tf
@@ -2,8 +2,8 @@
 
 #Creating VPC for initial Devops workstation
 #vpc
-resource "aws_vpc" "main" {
-  cidr_block =
-  enable_dns_support = 
-  enable_dns_hostnames = 
-}
+#resource "aws_vpc" "main" {
+ # cidr_block =
+  #enable_dns_support = 
+  #nable_dns_hostnames = 
+#}
